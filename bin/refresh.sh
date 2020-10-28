206ed6e54a44925960f8f09b48b32bbb9f27afb2
Ben Spencer <dangerous.ben@gmail.com>
2020-10-28 09:32:41 +0000

    Refresh script.

---
+++ bin/refresh.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+(cd ~/src/coursier/coursier && ./refresh.sh && cd ~/src/metals && ./refresh.sh && cd ~/src/bloop && ./refresh.sh) &
+
+(cd ~/src/emacs && ./refresh.sh; cd ~/src/git && ./refresh.sh; cd ~/src/rage && ./refresh.sh; cd ~/src/elixir-ls && ./refresh.sh) &
+
+repos=$(cat <<'EOF'
+firejail
+sangria-graphql
+skarnet
+suckless
+tla+
+typelevel
+twitter
+youtube-dl
+emacs-packages
+EOF
+)
+
+for d in $repos; do
+    echo pulling all in "$d"...
+    cd ~/src/"$d" && pull-all
+done
