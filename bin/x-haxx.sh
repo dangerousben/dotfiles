11b02fb53d000a1f05b175f5a2a8e726dac56f4e
Ben Spencer <dangerous.ben@gmail.com>
2022-07-10 14:41:18 +0100

    commit random crap as laptop dying

--- bin/x-haxx.sh
+++ bin/x-haxx.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-xrandr --output eDP-1 --scale 0.6x0.6
-xrandr --output HDMI-2 --pos 1152x-200 --rotate left
+# xrandr --output eDP-1 --scale 0.6x0.6
+# xrandr --output HDMI-2 --pos 1152x-200 --rotate left
+xrandr --output HDMI-2 --pos 0x0 --rotate normal #&& xrandr --output eDP-1 --off
